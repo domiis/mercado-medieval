6789b35fadc9074904b463e73588218f8d3e6c82
hmarinhoo <hellenmcordeiro@gmail.com>
2025-04-25 09:02:34 -0300

    model

---
+++ src/main/java/model/item.java
@@ -0,0 +1,57 @@
+package model;
+
+public class item {
+    private String nome;
+    private String tipo;
+    private String raridade;
+    private double valor;
+    private String dono;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getRaridade() {
+        return raridade;
+    }
+
+    public void setRaridade(String raridade) {
+        this.raridade = raridade;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public item(String nome, String tipo, String raridade, double valor, String dono) {
+        this.nome = nome;
+        this.tipo = tipo;
+        this.raridade = raridade;
+        this.valor = valor;
+        this.dono = dono;
+    }
+}
--- src/main/java/model/personagens.java
+++ src/main/java/model/personagens.java
@@ -1,5 +1,47 @@
 package model;
 
 public class personagens {
-    
+    private String nome;
+    private String classe;
+    private int nivel;
+    private int moedas;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getClasse() {
+        return classe;
+    }
+
+    public void setClasse(String classe) {
+        this.classe = classe;
+    }
+
+    public int getNivel() {
+        return nivel;
+    }
+
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+
+    public int getMoedas() {
+        return moedas;
+    }
+
+    public void setMoedas(int moedas) {
+        this.moedas = moedas;
+    }
+
+    public personagens(String nome, String classe, int nivel, int moedas) {
+        this.nome = nome;
+        this.classe = classe;
+        this.nivel = nivel;
+        this.moedas = moedas;
+    }
 }
